c34a5cb95a0a3f517e1470910411a745f89bb678
Priyanker <priyankerrao@gmail.com>
2024-05-26 16:04:26 -0600

    changing static settings.[y

--- weather_movie_recommender/weather_movie_recommender/settings.py
+++ weather_movie_recommender/weather_movie_recommender/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = ['movierecommendationbyweather.azurewebsites.net']
 
 INSTALLED_APPS = [
     'recommendations',
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -123,7 +124,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = ('whitenoise.storage.CompressedManifestStaticFilesStorage')
 STATIC_ROOT = BASE_DIR / 'static/recommendations'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
