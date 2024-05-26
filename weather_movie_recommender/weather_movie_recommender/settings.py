4c2b32f7a97eb099540f6babb896828a9c8b552c
Priyanker <priyankerrao@gmail.com>
2024-05-26 15:52:44 -0600

    static files location error

--- weather_movie_recommender/weather_movie_recommender/settings.py
+++ weather_movie_recommender/weather_movie_recommender/settings.py
@@ -124,7 +124,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'static/recommendations'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
