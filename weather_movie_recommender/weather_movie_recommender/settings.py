6ee4d896b4c27ffbdd36d8a2a81150194736070f
Priyanker <priyankerrao@gmail.com>
2024-05-26 15:20:31 -0600

    typo in STATICFILES_STORAGE

--- weather_movie_recommender/weather_movie_recommender/settings.py
+++ weather_movie_recommender/weather_movie_recommender/settings.py
@@ -123,7 +123,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-STATICFILES_STORAGE = 'whitenoise.storage. CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
